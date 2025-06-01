d55f00b639748ec369f0422f279a992851e6b7f8
Elməddinİsmayilov <elmeddin.e.ismayilov@gmail.com>
2025-06-01 16:52:53 +0400

    Create README.d

---
+++ README.d
@@ -0,0 +1,19 @@
+# No Smoking
+
+Bu layihə siqaretin zərərlərinə diqqət çəkən sadə vizual layihədir. HTML və CSS vasitəsilə hazırlanmışdır.
+
+## Məqsəd
+Layihənin məqsədi siqaretin zərərləri barədə məlumat vermək və istifadəçiləri siqaretdən uzaq durmağa təşviq etməkdir.
+
+## Texnologiyalar
+- HTML5  
+- CSS3  
+
+## Xüsusiyyətlər
+- Statik vizual təqdimat  
+- Mesaj və xəbərdarlıq elementləri  
+- Təmiz və oxunaqlı dizayn
+
+  
+## Müəllif
+Elmeddin Ismayilov
